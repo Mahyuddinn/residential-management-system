f9d147e883d7ab1f4fca588aa34e84d08615bd72
Mahyuddinn <ahmadmahyuddin@graduate.utm.my>
2024-07-02 21:41:34 +0800

    2/7/2024

---
+++ functions/index.js
@@ -0,0 +1,75 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const {onSchedule} = require("firebase-functions/v2/scheduler");
+const logger = require("firebase-functions/logger");
+const admin = require("firebase-admin");
+
+// Initialize Firebase Admin SDK
+admin.initializeApp();
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+const db = admin.firestore();
+
+// Function to add maintenance fees monthly
+exports.addMonthlyMiantenanceFee = onSchedule("0 0 * * *", async (context) => {
+  try {
+    const now = admin.firestore.Timestamp.now();
+    const batch = db.batch();
+    const residentRef = db.collection("Resident");
+
+    const snapshot = await residentRef.get();
+    snapshot.forEach((doc) => {
+      const docRef = residentRef.doc(doc.id);
+      batch.update(docRef, {
+        maintenanceFees: admin.firestore.FieldValue.arrayUnion({
+          amount: 1,
+          date: now,
+        }),
+      });
+    });
+
+    await batch.commit();
+    console.log("Maintenance fee added to all resident at:", now.toDate());
+  } catch (error) {
+    logger.error("Error adding the maintenance fees:", error);
+  }
+});
+
+exports.callback = onRequest((request, response) => {
+  // Check if the request method is POST
+  if (request.method !== "POST") {
+    logger.error("Method Not Allowed");
+    return response.status(405).send("Method Not Allowed");
+  }
+
+  // Log the received data
+  const data = request.body;
+  logger.info("Callback received:", data);
+
+  // Save the data to Firestore
+  const db = admin.firestore();
+  db.collection("callbacks").add(data)
+      .then((docRef) => {
+        logger.info("Document written with ID:", docRef.id);
+        response.status(200).send("Callback received and logged");
+      })
+      .catch((error) => {
+        logger.error("Error adding document:", error);
+        response.status(500).send("Error logging callback");
+      });
+});
--- lib/features/View/payMaintananceFee.dart
+++ lib/features/View/payMaintananceFee.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:residential_management_system/features/View/home_page.dart';
@@ -18,33 +20,247 @@ class _BillingPageState extends State<BillingPage> {
   final String _apiKey = 'ac3bde40-da73-4895-ba1e-8498bd71b47b';
   final String _collectionId = 'rraupvz0';
   bool _isLoading = false;
+  String? _userName;
+  double _totalMaintenanceFees = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserName();
+    _fetchTotalMaintenanceFees();
+  }
+
+  Future<void> _fetchUserName() async {
+    try {
+      print('USER EMAIL: $user.email');
+      DocumentSnapshot doc = await FirebaseFirestore.instance
+          .collection('Resident')
+          .doc(user.email)
+          .get();
+
+      if (doc.exists) {
+        setState(() {
+          _userName = doc['name'];
+        });
+        print('User name fetched successfully: $_userName');
+      } else {
+        print('Document does not exist');
+      }
+    } catch (e) {
+      print('Error fetching user name: $e');
+    }
+  }
+
+  Future<void> _fetchTotalMaintenanceFees() async {
+    FirebaseFirestore.instance
+        .collection('Resident')
+        .doc(user.email)
+        .snapshots()
+        .listen((docSnapshot) {
+      if (docSnapshot.exists) {
+        var data = docSnapshot.data() as Map<String, dynamic>;
+        if (data.containsKey('totalMaintenanceFees')) {
+          setState(() {
+            _totalMaintenanceFees = data['totalMaintenanceFees'] ?? 0.0;
+          });
+          print('Fetched total maintenance fees: $_totalMaintenanceFees');
+        } else {
+          print('totalMaintenanceFees field does not exist, initializing...');
+          _initializeTotalMaintenanceFees();
+        }
+      } else {
+        print('Document does not exist, initializing...');
+        _initializeTotalMaintenanceFees();
+      }
+    }, onError: (error) {
+      print('Error fetching total maintenance fees: $error');
+    });
+  }
+
+  Future<void> _initializeTotalMaintenanceFees() async {
+    try {
+      DocumentReference docRef =
+          FirebaseFirestore.instance.collection('Resident').doc(user.email);
+
+      // First, get the current document data
+      DocumentSnapshot docSnap = await docRef.get();
+      Map<String, dynamic> data = docSnap.data() as Map<String, dynamic>? ?? {};
+
+      // If totalMaintenanceFees doesn't exist, calculate it from maintenanceFees
+      if (!data.containsKey('totalMaintenanceFees')) {
+        List<dynamic> maintenanceFees = data['maintenanceFees'] ?? [];
+        double total = maintenanceFees.fold(
+            0.0, (sum, fee) => sum + (fee['amount'] ?? 0.0));
+
+        // Update the document with the calculated totalMaintenanceFees
+        await docRef.set({
+          'totalMaintenanceFees': total,
+        }, SetOptions(merge: true));
+
+        setState(() {
+          _totalMaintenanceFees = total;
+        });
+      } else {
+        // If totalMaintenanceFees already exists, just use its value
+        setState(() {
+          _totalMaintenanceFees = data['totalMaintenanceFees'] ?? 0.0;
+        });
+      }
+
+      print(
+          'Initialized total maintenance fees successfully: $_totalMaintenanceFees');
+    } catch (e) {
+      print('Error initializing total maintenance fees: $e');
+    }
+  }
 
   Future<void> _createBill() async {
-    final String url = 'https://www.billplz-sandbox.com/api/v3/bills';
-    final response = await http.post(
-      Uri.parse(url),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Basic ${base64Encode(utf8.encode("$_apiKey:"))}',
-      },
-      body: jsonEncode({
-        'collection_id': _collectionId,
-        'email': user.email,
-        'name': user.displayName,
-        'amount': int.parse(_amountController.text) * 100,
-        'callback_url': '',
-        'description': 'Maintenance Fee',
-      }),
-    );
+    if (_userName == null) {
+      print('User name is not available');
+      return;
+    }
 
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> bill = jsonDecode(response.body);
-      final String paymentUrl = bill['url'];
-      // Open the payent URL in a web view or browser
-      _openPaymentUrl(paymentUrl);
-    }else {
-      // Handle the error
-      print('Error creating bill: ${response.body}');
+    double paidAmount = double.tryParse(_amountController.text) ?? 0.0;
+    if (paidAmount <= 0 || paidAmount > _totalMaintenanceFees) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Invalid payment amount')),
+      );
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final String url = 'https://www.billplz-sandbox.com/api/v3/bills';
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Basic ${base64Encode(utf8.encode("$_apiKey:"))}',
+        },
+        body: jsonEncode({
+          'collection_id': _collectionId,
+          'email': user.email,
+          'name': _userName,
+          'amount': (paidAmount * 100).toInt(),
+          'callback_url': 'https://callback-nnvacxxqwa-uc.a.run.app',
+          'description': 'Maintenance Fee',
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> bill = jsonDecode(response.body);
+        final String paymentUrl = bill['url'];
+        final String billId = bill['id'];
+        print('Payment URL: $paymentUrl');
+
+        _openPaymentUrl(paymentUrl);
+        _pollPaymentStatus(billId, paidAmount);
+
+        // Clear the text field after successful bill creation
+        _amountController.clear();
+      } else {
+        print('Error creating bill: ${response.body}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to create bill. Please try again.')),
+        );
+      }
+    } catch (e) {
+      print('Error in _createBill: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('An error occurred. Please try again.')),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<void> _updateTotalMaintenanceFees() async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('Resident')
+          .doc(user.email)
+          .update({
+        'totalMaintenanceFees': _totalMaintenanceFees,
+      });
+      print(
+          'Total maintenance fees updated successfully: $_totalMaintenanceFees');
+    } catch (e) {
+      print('Error updating total maintenance fees: $e');
+    }
+  }
+
+  void _pollPaymentStatus(String billId, double paidAmount) {
+    Timer.periodic(Duration(seconds: 10), (timer) async {
+      final String url = 'https://www.billplz-sandbox.com/api/v3/bills/$billId';
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'Authorization': 'Basic ${base64Encode(utf8.encode("$_apiKey:"))}',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> bill = jsonDecode(response.body);
+        final bool isPaid = bill['paid'];
+        if (isPaid) {
+          timer.cancel();
+          // Update the UI or Firestore
+          await _updatePaymentStatus(billId, isPaid, paidAmount);
+          print('Payment Succesful');
+
+          // Refresh the total maintenance fees
+          await _fetchTotalMaintenanceFees();
+        }
+      } else {
+        print('Error fethching bill status: ${response.body}');
+      }
+    });
+  }
+
+  Future<void> _updatePaymentStatus(
+      String billId, bool isPaid, double paidAmount) async {
+    try {
+      await FirebaseFirestore.instance.runTransaction((transaction) async {
+        DocumentReference docRef =
+            FirebaseFirestore.instance.collection('Resident').doc(user.email);
+        DocumentSnapshot docSnapshot = await transaction.get(docRef);
+
+        if (docSnapshot.exists) {
+          var data = docSnapshot.data() as Map<String, dynamic>;
+          List<dynamic> feesList = data['maintenanceFees'] ?? [];
+          double currentTotal = data['totalMaintenanceFees'] ?? 0.0;
+          double newTotal = currentTotal - paidAmount;
+
+          // Update payment status in feesList
+          for (var fee in feesList) {
+            if (!fee.containsKey('paid') || fee['paid'] == false) {
+              fee['paid'] = isPaid;
+              break;
+            }
+          }
+
+          transaction.update(docRef, {
+            'maintenanceFees': feesList,
+            'totalMaintenanceFees': newTotal,
+          });
+
+          transaction.set(docRef.collection('bills').doc(billId), {
+            'payment_status': isPaid ? 'paid' : 'unpaid',
+            'bill_id': billId,
+            'amount': paidAmount,
+            'timestamp': FieldValue.serverTimestamp(),
+          });
+        }
+      });
+
+      print('Payment status and total maintenance fees updated successfully');
+    } catch (e) {
+      print('Error updating payment status and total maintenance fees: $e');
     }
   }
 
@@ -64,18 +280,72 @@ class _BillingPageState extends State<BillingPage> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            TextField(
-              controller: _amountController,
-              decoration: InputDecoration(
-                labelText: 'Amount (RM)',
-                border: OutlineInputBorder(),
+            Card(
+              elevation: 4,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Total Maintenance Fees',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'RM ${_totalMaintenanceFees.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              keyboardType: TextInputType.number,
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _createBill,
-              child: Text('Pay Maintenance Fee'),
+            Card(
+              elevation: 4,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'Make Payment',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    TextField(
+                      controller: _amountController,
+                      decoration: InputDecoration(
+                        labelText: 'Amount (RM)',
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                    SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: _createBill,
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Text(
+                          'Pay Maintenance Fee',
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
@@ -98,23 +368,11 @@ class _BillingPageState extends State<BillingPage> {
           SizedBox(
             height: 5,
           ),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomePage()),
-              );
-            },
-            child: Image.asset(
-              'assets/images/sos.png',
-              height: 120,
-            ),
-          ),
         ],
       ),
       centerTitle: false,
       backgroundColor: Colors.redAccent[700],
-      toolbarHeight: 200,
+      toolbarHeight: 100,
       flexibleSpace: Container(),
     );
   }
